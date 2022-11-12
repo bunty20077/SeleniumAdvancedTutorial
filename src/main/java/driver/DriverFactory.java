86540201b9f337fb1597312cfa8e8c66a748b026
bunty20077 <sayantan20077@rediffmail.com>
2022-11-12 20:51:49 +0530

    Cloud Testing and logic for service added

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -6,10 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public final class DriverFactory {
 
 
+    private static String username = "XXXXXXX";
+    private static String accesskey = "XXXXXXxxxxxXXXXXXxxxxxxXXXX";
+    private static String gridURL = "@hub.lambdatest.com/wd/hub";
 
     /*
      * Factory method for getting driver and browsers
@@ -25,25 +33,47 @@ public final class DriverFactory {
 
     public static WebDriver getDriver() {
         String browserName = System.getProperty("browser");
-        if (browserName == null) {
+        String service = System.getProperty("service");
+
+        if (browserName == null || service == null) {
             browserName = "chrome";
+            service = "local";
         }
+
         WebDriver driver = null;
-        switch (browserName) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                System.out.println("I am at FF");
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "edge":
-                System.out.println("I am at Edge");
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
+        if (service.equalsIgnoreCase("local")) {
+            switch (browserName) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    System.out.println("I am at FF");
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    System.out.println("I am at Edge");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+            }
+        } else if (service.equalsIgnoreCase("docker")) {
+
+        } else if (service.equalsIgnoreCase("cloud")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "chrome");
+            // capabilities.setCapability("version", "70.0");
+            capabilities.setCapability("platform", "win10"); // If this cap isn't specified, it will just get the any available one
+            capabilities.setCapability("build", "LambdaTestSampleApp");
+            capabilities.setCapability("name", "LambdaTestJavaSample");
+            try {
+                driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+            } catch (MalformedURLException e) {
+                System.out.println("Invalid grid URL");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return driver;
     }
